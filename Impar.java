2d70b71a0f319e9482675128f13802685f92933a
luana <luanab.moura125@gmail.com>
2024-09-20 10:21:47 -0300

    Impar.java

---
+++ Impar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class impar {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int v;
+
+        System.out.println("Digite o valor:");
+        v = teclado.nextInt();
+
+        if (v % 2 == 0) {
+            System.out.println("Valor par");
+        } else {
+            System.out.println("Valor ímpar");
+        }
+
+        teclado.close();
+    }
+}
