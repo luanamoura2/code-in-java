e42d037ea3ee767ef786805e6d0d6665f0310a57
luana <luanab.moura125@gmail.com>
2024-09-20 10:21:15 -0300

    Idade.java

---
+++ Idade.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class idade {
+
+  public static void main(String[] args) {
+
+    Scanner teclado = new Scanner(System.in);
+    int ida;
+
+    System.out.println("Digite sua idade");
+    ida = teclado.nextInt();
+
+    if ((ida <= 11)) {
+      System.out.println("Você é uma criança");
+    } else {
+      if ((ida >= 12 && ida <= 17)) {
+        System.out.println("Você é um adolescente");
+      } else {
+
+        if ((ida >= 18 && ida <= 59)) {
+          System.out.println("Você é um adulto");
+
+        } else {
+
+          System.out.println("Você é um idoso");
+        }
+
+      }
+    }
+    teclado.close();
+  }
+
+}
+
