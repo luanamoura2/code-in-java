39a63ab94fb33cdb798c4a3f48f6d173ec731243
luana <luanab.moura125@gmail.com>
2024-09-20 10:22:29 -0300

    Matriz transporta.java

---
+++ Matriz transporta.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class MatrizTransposta {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        Random gerador = new Random();
+        int Matriz[][] = new int[5][5];
+        int i, j;
+        for (i = 0; i <= 4; i++) {
+            for (j = 0; j <= 4; j++) {
+                Matriz[i][j] = gerador.nextInt(10, 50);
+            }
+
+        }
+        for (i = 0; i <= 4; i++) {
+            System.out.print("\n");
+            for (j = 0; j <= 4; j++) {
+                System.out.print(Matriz[i][j] + " ");
+            }
+
+            teclado.close();
+        }
+        System.out.print("\n");
+
+        for (j = 0; j <= 4; j++) {
+            System.out.print("\n");
+            for (i = 0; i <= 4; i++) {
+                System.out.print(Matriz[i][j] + " ");
+            }
+
+        }
+    }
+}
+
