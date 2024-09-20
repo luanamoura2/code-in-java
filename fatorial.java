0275296f102c3fe0bb08ccfd492aa7edc02f45d6
luana <luanab.moura125@gmail.com>
2024-09-20 10:17:36 -0300

    Fatorial.java

---
+++ fatorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class fatorial {
+
+  public static void main(String[] args) {
+    int vet1[] = new int[5];
+    int i,j;
+    int fatorial = 1;
+    Scanner teclado = new Scanner(System.in);
+    for (i = 0; i < 5; i++) {
+      System.out.println("Digite um valor:");
+      vet1[i] = teclado.nextInt();
+      for (j = 1; j<= vet1[i]; j++) {
+          fatorial=fatorial * j;
+      }
+        System.out.println("Fatorial do número é: " + fatorial); 
+        fatorial=1;
+    }
+
+  }
+}
+
